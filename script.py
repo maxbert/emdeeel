9e21f4e6aac06286343603d7d7f30098508890a4
max Bertfield <mbertfield@stuy.edu>
2017-05-08 22:35:14 -0400

    done

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ def run(filename):
 
     p = mdl.parseFile(filename)
     step = 0.1
-    systems = [ temp ]
+    systems = [ tmp ]
     if p:
         (commands, symbols) = p
     else:
@@ -27,17 +27,24 @@ def run(filename):
     screen = new_screen()
     tmp = []
     step = 0.1
+    line = ""
     clear_screen(screen)
     for command in commands:
+        line = command[0]
+        edges = stack[0]
         if command[0] in ARG_COMMANDS:
             args = command[1:]
-
-        if command[0] == "sphere":
-            add_sphere(edges,float(args[0]), float(args[1]), float(args[2]),float(args[3]), step)
+            
+        if line == 'sphere':
+                #print 'SPHERE\t' + str(args)
+            add_sphere(edges,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
             matrix_mult( systems[-1], edges )
             draw_polygons(edges, screen, color)
             edges = []
-        elif command[0] == 'torus':
+
+        elif line == 'torus':
             #print 'TORUS\t' + str(args)
             add_torus(edges,
                       float(args[0]), float(args[1]), float(args[2]),
@@ -45,7 +52,8 @@ def run(filename):
             matrix_mult( systems[-1], edges )
             draw_polygons(edges, screen, color)
             edges = []
-        elif command[0] == 'box':
+            
+        elif line == 'box':
             #print 'BOX\t' + str(args)
             add_box(edges,
                     float(args[0]), float(args[1]), float(args[2]),
@@ -53,13 +61,14 @@ def run(filename):
             matrix_mult( systems[-1], edges )
             draw_polygons(edges, screen, color)
             edges = []
+            
         elif line == 'circle':
             #print 'CIRCLE\t' + str(args)
             add_circle(edges,
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step)
 
-        elif command == 'hermite' or command == 'bezier':
+        elif line == 'hermite' or line == 'bezier':
             #print 'curve\t' + line + ": " + str(args)
             add_curve(edges,
                       float(args[0]), float(args[1]),
@@ -67,11 +76,59 @@ def run(filename):
                       float(args[4]), float(args[5]),
                       float(args[6]), float(args[7]),
                       step, line)                      
-        elif command == 'line':            
+            
+        elif line == 'line':            
             #print 'LINE\t' + str(args)
 
             add_edge( edges,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
 
+        elif line == 'scale':
+            #print 'SCALE\t' + str(args)
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( systems[-1], t )
+            systems[-1] = [ x[:] for x in t]
+
+        elif line == 'move':
+            #print 'MOVE\t' + str(args)
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( systems[-1], t )
+            systems[-1] = [ x[:] for x in t]
+
+
+        elif line == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            theta = float(args[1]) * (math.pi / 180)
+            
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+                matrix_mult( systems[-1], t )
+                systems[-1] = [ x[:] for x in t]
+                
+        elif line == 'clear':
+            edges = []
+            
+        elif line == 'ident':
+            ident(transform)
+
+        elif line == 'apply':
+            matrix_mult( transform, edges )
+
+        elif line == 'push':
+            systems.append( [x[:] for x in systems[-1]] )
+            
+        elif line == 'pop':
+            systems.pop()
+            
+        elif line == 'display' or line == 'save':
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
+                 
         print command
